d388462b541f14bc121bbce4310d275dd52509e9
Elhan Mahmutovic <elhan.mahmutovic03@gmail.com>
2025-02-22 21:47:02 +0100

    format

--- models/map_user.go
+++ models/map_user.go
@@ -3,13 +3,13 @@ package models
 import "time"
 
 type MapUser struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"name"`
-	Avatar    string    `json:"avatar"`
-	Latitude  float64   `json:"latitude"`
-	Longitude float64   `json:"longitude"`
-	Activity  string    `json:"activity"`
-	Timestamp time.Time `json:"timestamp"`
-	IconType  string    `json:"iconType"`
-	EmissionAmount  float64   `json:"emission_amount"`
+	ID             uint      `json:"id" gorm:"primaryKey"`
+	Name           string    `json:"name"`
+	Avatar         string    `json:"avatar"`
+	Latitude       float64   `json:"latitude"`
+	Longitude      float64   `json:"longitude"`
+	Activity       string    `json:"activity"`
+	Timestamp      time.Time `json:"timestamp"`
+	IconType       string    `json:"iconType"`
+	EmissionAmount float64   `json:"emission_amount"`
 }
